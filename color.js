0ae4bde010c0bb63e90a9e4896428abbe52f7855
carlozaul25 <carloz18rivera@live.com>
2018-10-06 21:45:05 -0400

    pick a color

--- color.js
+++ color.js
@@ -8,7 +8,25 @@ var colors = [
 	]
 
 var squares = document.querySelectorAll(".squares");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById('colorDisplay');
+
+colorDisplay.textContent = pickedColor;
 
 for(i=0; i<squares.length; i++) {
+	//add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
+
+	//add click listeners to the squares
+	squares[i].addEventListener("click", function() {
+		//grab color of picked square
+		var clickedColor = this.style.backgroundColor;
+		//compare color to picked color
+		if(clickedColor === pickedColor) {
+			alert('correct')
+		}else{
+			alert('wrong')
+		}
+	});
 }
+
