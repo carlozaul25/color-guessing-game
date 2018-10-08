48539d84bf6b1d7b3a035baeeb5d9e9fd0075c51
carlozaul25 <carloz18rivera@live.com>
2018-10-08 13:29:33 -0400

    picking random colors from the array

--- color.js
+++ color.js
@@ -8,7 +8,7 @@ var colors = [
 	]
 
 var squares = document.querySelectorAll(".squares");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector("#message");
 
@@ -25,10 +25,27 @@ for(i=0; i<squares.length; i++) {
 		//compare color to picked color
 		if(clickedColor === pickedColor) {
 			messageDisplay.textContent = 'Correct!';
+			changeColors(clickedColor);
 		}else{
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = 'Try again';
 		}
 	});
 }
+
+function changeColors(color) {
+	for(i=0; i<squares.length; i++) {
+		//changing the colors to match given color
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+
+
+
 
