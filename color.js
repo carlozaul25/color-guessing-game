1b53254e2150221c010bfcc9b92acdde34e0c6cd
carlozaul25 <carloz18rivera@live.com>
2018-10-12 17:02:51 -0400

    styling of buttons and fonts

--- color.js
+++ color.js
@@ -1,4 +1,4 @@
-var colors = generateRandomColors(6);
+var colors = generateRandomColors(numOfSquares);
 var squares = document.querySelectorAll(".squares");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
@@ -7,12 +7,14 @@ var h1 = document.querySelector('h1');
 var resetButton = document.querySelector('#reset');
 var easyBtn = document.querySelector('#easyBtn');
 var hardBtn = document.querySelector('#hardBtn');
+var numOfSquares = 6;
 
 //easy button
 easyBtn.addEventListener('click', function() {
 	hardBtn.classList.remove ('selected');
 	easyBtn.classList.add ('selected');
-	colors = generateRandomColors(3);
+	numOfSquares = 3;
+	colors = generateRandomColors(numOfSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
 	for(i=0; i<squares.length; i++) {
@@ -28,7 +30,8 @@ easyBtn.addEventListener('click', function() {
 hardBtn.addEventListener('click', function() {
 	hardBtn.classList.add ('selected');
 	easyBtn.classList.remove ('selected');
-	colors = generateRandomColors(3);
+	numOfSquares = 6;
+	colors = generateRandomColors(numOfSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
 	for(i=0; i<squares.length; i++) {
@@ -40,7 +43,7 @@ hardBtn.addEventListener('click', function() {
 	//reset button
 resetButton.addEventListener('click', function() {
 	//generate all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numOfSquares);
 	//pick a new random color from array
 	pickedColor = pickColor();
 	//change color display to match picked color
@@ -49,7 +52,7 @@ resetButton.addEventListener('click', function() {
 	for(i=0; i<squares.length; i++) {
 	squares[i].style.backgroundColor = colors[i];
 	}
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
 
 });
 
